b7eeba287a2cdd5a3c2f8dff63919cb7ddda6071
Naveen LEE <gyi5566@gmail.com>
2020-07-21 21:28:12 +0700

    Add new file

---
+++ c_to_f.py
@@ -0,0 +1,11 @@
+c = input('請使用者輸入攝氏溫度: ')
+c = float(c)
+
+f = c * 9 / 5 + 32
+print('華氏溫度', f)
+
+fahrenheit = input('請輸入華氏溫度: ')
+fahrenheit = float(fahrenheit)
+celsius = 5 / 9 * (fahrenheit - 32)
+
+print('攝氏溫度', celsius)
